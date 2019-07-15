c225043c2495cfbf459b1ce73077210e0e26d7ce
allen <allen@weidun.biz>
2019-07-15 17:38:05 +0800

    fix code

--- example/config.php
+++ example/config.php
@@ -5,7 +5,7 @@ const CLIENT_ID = '10002';
 const APP_KEY = 'k1qn32lw9ppdfmxslch1b8rh9pvrcdfw';
 
 \MasJPay\MasJPay::setDebug(true); //调试模式   true /false
-\MasJPay\MasJPay::setApiMode('sandbox'); //环境  live 线上，sandbox 沙盒
+\MasJPay\MasJPay::setApiMode('live'); //环境  live 线上，sandbox 沙盒
 \MasJPay\MasJPay::setClientId(CLIENT_ID);   // 设置 CLIENT ID
 \MasJPay\MasJPay::setApiKey(APP_KEY);    // 设置 API Key
 
